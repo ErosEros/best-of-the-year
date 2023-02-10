009b206ea8f596d262f9d9018570c359bd486c31
ErosEros <eros.vittori1994@gmail.com>
2023-02-10 17:52:42 +0100

    milestone 2 completed

---
+++ src/main/java/com/example/demo/Song.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+public class Song {
+	
+	private String title;
+	private int id;
+	
+	//	COSTRUTTORE
+	public Song(String title, int id) {
+		super();
+		this.title = title;
+		this.id = id;
+	}
+	
+//	GETTERS E SETTERS
+	
+public String getTitle() {
+		return title;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Movie [title=" + title + "]"; 
+	}
+	
+}
--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,9 +1,15 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.Movie;
+import com.example.demo.Song;
 
 @Controller
 @RequestMapping("/nome")
@@ -14,6 +20,58 @@ public class MainController {
 	public String home(Model model) { 
 		
 		model.addAttribute("name" , "Eros");
-		return "index";
+		return "index";		
 	}
+	
+//	ROTTA MOVIES
+	@GetMapping("/movies")
+	public String movie(Model model) {
+		List<Movie> movies = getBestMovies();
+		model.addAttribute("movies", movies);
+		return "movies";
+	}
+
+//	ROTTA MOVIES
+	@GetMapping("/songs")
+	public String song(Model model) {
+		List<Song> songs = getBestSongs();
+		model.addAttribute("songs", songs);
+		return "songs";
+	}
+	
+//	METODO PER PRENDERE I MOVIE
+	private List<Movie> getBestMovies(){
+//		inizializzo la lista di array
+		List<Movie> movies = new ArrayList<>();
+		
+//      creo il film 1		
+		Movie movie1 = new Movie("Batman", 1);
+		movies.add(movie1);
+
+//      creo il film 2	
+		Movie movie2 = new Movie("One-Piece", 2);
+		movies.add(movie2);
+		
+		return movies;
+		
+	}
+	
+	
+//	METODO PER PRENDERE LE SONGS
+	private List<Song> getBestSongs(){
+//		inizializzo la lista di array
+		List<Song> songs = new ArrayList<>();
+		
+//      creo la song 1		
+		Song song1 = new Song("Rolls Royce", 1);
+		songs.add(song1);
+
+//      creo la song2	
+		Song song2 = new Song("Cenere", 2);
+		songs.add(song2);
+		
+		return songs;
+		
+	}
+	
 }
